cfee43b95e17808fb3ca69f41bfb3952675dfbaf
Роман Смирнов <pombl4.01@mail.ru>
2022-05-20 13:40:58 +0300

    sfml added

--- flappyBird/main.cpp
+++ flappyBird/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include "SFML/Window.hpp"
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
 int main() {
-	cout << "tipo game" << endl;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "sue");
+
+    sf::CircleShape bird;
+    bird.setFillColor(sf::Color::Red);
+    bird.setRadius(20);
+    bird.setPosition(sf::Vector2f(400, 300));
+	
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Blue);
+        window.draw(bird);
+        window.display();
+    }
+
 	return 0;
 }
