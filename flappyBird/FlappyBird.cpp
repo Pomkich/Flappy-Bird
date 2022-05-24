50190153b5fda9aa4f5c69cc3aabde7b7b2f4310
Роман Смирнов <pombl4.01@mail.ru>
2022-05-24 23:09:01 +0300

    added comments to flappy bird

--- flappyBird/FlappyBird.cpp
+++ flappyBird/FlappyBird.cpp
@@ -6,18 +6,20 @@ FlappyBird::FlappyBird() {
 }
 
 void FlappyBird::HandleTiming(double elapsed) {
+	// check all events
 	for (auto& time_event : events) {
 		int id; double timing; void (FlappyBird::*method_ptr)();
-		std::tie(id, timing, method_ptr) = time_event;
-		if (timing < 0) {
-			(shared_from_this().get()->*method_ptr)();
-			event_delete_list.push_back(id);
+		std::tie(id, timing, method_ptr) = time_event;	// unpack elements of tuple
+		if (timing < 0) {	// if time expires, call function
+			(shared_from_this().get()->*method_ptr)();	
+			event_delete_list.push_back(id);	// link event to delete
 		}
-		else {
+		else {	// else decrease timing
 			std::get<1>(time_event) -= elapsed;
 		}
 	}
 
+	// delete events, that has called higher
 	for (auto& delete_id : event_delete_list) {
 		events.erase(std::remove_if(events.begin(), events.end(),
 			[delete_id](std::tuple<int, double, void (FlappyBird::*)()> ev) { 
--- flappyBird/FlappyBird.h
+++ flappyBird/FlappyBird.h
@@ -10,6 +10,10 @@ private:
 	int current_pipe;
 	std::shared_ptr<Bird> bird;
 	std::vector<std::shared_ptr<GameObject>> pipes;
+	// events, that would call after the expiration of time
+	// first element of tuple - id, needed for identifying event and deleting him
+	// second - timing. It decreases every iteration
+	// third - pointer to the method to be called
 	std::vector<std::tuple<int, double, void (FlappyBird::*)()>> events;
 	std::list<int> event_delete_list;
 
