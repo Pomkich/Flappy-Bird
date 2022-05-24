3e995ae9aa4d8bb28c3e68374e9e5646ee5dccad
Роман Смирнов <pombl4.01@mail.ru>
2022-05-24 21:10:45 +0300

    small bug fixing + refactor + comments

--- flappyBird/SGameEngine.cpp
+++ flappyBird/SGameEngine.cpp
@@ -16,6 +16,7 @@ void SGameEngine::addObject(std::shared_ptr<GameObject> p_obj) {
     game_objects.push_back(p_obj);
 }
 
+// cannot be deleted right now, deletion will occur at the end of the game cycle
 void SGameEngine::deleteObject(std::string obj_name) {
     if (!game_objects.empty()) {
         delete_list.push_back(obj_name);
@@ -30,9 +31,9 @@ void SGameEngine::HandleInput(sf::Event input) {
     }
 }
 
-// additional function that calculates intersection of segments
-bool otrezok(double x11, double x12, double x21, double x22) {
-    if ((x11 > x21 && x11 < x22) || (x12 > x21 && x12 < x22)) {
+//the function considers whether the smaller segment touches the larger one
+bool largerIntersects(double x11, double x12, double x21, double x22) {
+    if ((x11 >= x21 && x11 <= x22) || (x12 >= x21 && x12 <= x22)) {
         return true;
     }
     return false;
@@ -45,14 +46,19 @@ void SGameEngine::CheckCollision() {
                 auto rect1 = game_objects[checkable_obj]->getBoundingRect();    // getting rectangles bounds
                 auto rect2 = game_objects[obj_oncheck]->getBoundingRect();
                 
+                // getting the x coordinates of each rectangle
                 double x_left1 = rect1.getPosition().x, x_right1 = rect1.getPosition().x + rect1.getSize().x;
                 double x_left2 = rect2.getPosition().x, x_right2 = rect2.getPosition().x + rect2.getSize().x;
-
+                // and y coordinate
                 double y_left1 = rect1.getPosition().y, y_right1 = rect1.getPosition().y + rect1.getSize().y;
                 double y_left2 = rect2.getPosition().y, y_right2 = rect2.getPosition().y + rect2.getSize().y;
 
-                if ((otrezok(x_left1, x_right1, x_left2, x_right2) || otrezok(x_left2, x_right2, x_left1, x_right1)) &&
-                    (otrezok(y_left1, y_right1, y_left2, y_right2) || otrezok(y_left2, y_right2, y_left1, y_right1))) {
+                // an intersection is considered found if the segments on the x and y axes intersect.
+                // Since the function counts only the touch of the smaller segment of the larger one, it 
+                // should be called for two cases. In one of them, the segment will be less than or equal 
+                // to the second
+                if ((largerIntersects(x_left1, x_right1, x_left2, x_right2) || largerIntersects(x_left2, x_right2, x_left1, x_right1)) &&
+                    (largerIntersects(y_left1, y_right1, y_left2, y_right2) || largerIntersects(y_left2, y_right2, y_left1, y_right1))) {
                     auto center_rect1 = rect1.getPosition();
                     auto center_rect2 = rect2.getPosition();
 
@@ -61,6 +67,9 @@ void SGameEngine::CheckCollision() {
                     center_rect2.x = center_rect2.x + (rect2.getSize().x / 2);
                     center_rect2.y = center_rect2.y + (rect2.getSize().y / 2);
 
+                    // comparing the coordinates of the rectangles, 
+                    // we can conclude about the mutual location between them.
+                    // The handler call is called for two cases, on the x and y axis
                     Side side1 = Side::left, side2 = Side::right;
                     if (center_rect1.x > center_rect2.x) {
                         side1 = Side::left; side2 = Side::right;
@@ -107,6 +116,7 @@ void SGameEngine::Render() {
 }
 
 void SGameEngine::DeleteObjects() {
+    if (delete_list.empty()) return;
     for (auto& obj_name : delete_list) {
         game_objects.erase(std::remove_if(game_objects.begin(), game_objects.end(),
             [obj_name](std::shared_ptr<GameObject> obj) { return obj->getName() == obj_name; }),
@@ -118,7 +128,7 @@ void SGameEngine::DeleteObjects() {
 
 void SGameEngine::Start() {
     window.setActive(true);
-    window.setKeyRepeatEnabled(false); // better disable for better perfomance
+    window.setKeyRepeatEnabled(false); // disable for better perfomance
 
     sf::Time elapsed;
     sf::Time lag = sf::milliseconds(0);
@@ -128,7 +138,7 @@ void SGameEngine::Start() {
         {   // start of critical section
             std::unique_lock<std::mutex> lock(synch);
             elapsed = clock.restart();
-            lag += elapsed;
+            lag += elapsed; // measure time between frames
             // check all the window's events that were triggered since the last iteration of the loop
             sf::Event input;
             while (window.pollEvent(input))
@@ -139,7 +149,7 @@ void SGameEngine::Start() {
                 }
             }
             CheckCollision();
-            while (lag.asMilliseconds() > UPDATE_RATE) {
+            while (lag.asMilliseconds() > UPDATE_RATE) {    // "catching up" with the correct state of the game
                 Update();
                 lag -= sf::milliseconds(UPDATE_RATE);
             }
